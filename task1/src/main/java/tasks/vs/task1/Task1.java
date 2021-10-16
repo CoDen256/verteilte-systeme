61051fb6a19a09f7bc6691d857dc22de81322bb3
Denys Chernyshov <den.blackshov@gmail.com>
2021-10-16 09:17:34 +0200

    Add task1 and task2 module

---
+++ task1/src/main/java/tasks/vs/task1/Task1.java
@@ -0,0 +1,7 @@
+package tasks.vs.task1;
+
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("Helo");
+    }
+}
---
+++ task2/src/main/java/tasks/vs/task2/Task2.java
@@ -0,0 +1,7 @@
+package tasks.vs.task2;
+
+public class Task2 {
+    public static void main(String[] args) {
+        System.out.println("Tanlo");
+    }
+}
